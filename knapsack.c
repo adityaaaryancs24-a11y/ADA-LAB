67b34045d1b7e9d1a28ef1b95c17fc2e48cb89f1
adityaaaryancs24-a11y <adityaaaryan.cs24@bmsce.ac.in>
2026-05-25 10:20:10 +0530

    Add files via upload

---
+++ knapsack.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int knapsack(int W, int wt[], int val[], int n) {
+
+    int dp[n + 1][W + 1];
+
+    // Build table dp[][]
+    for (int i = 0; i <= n; i++) {
+
+        for (int w = 0; w <= W; w++) {
+
+            // Base case
+            if (i == 0 || w == 0) {
+                dp[i][w] = 0;
+            }
+
+            // If item can be included
+            else if (wt[i - 1] <= w) {
+
+                dp[i][w] = max(
+                    val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                    dp[i - 1][w]
+                );
+            }
+
+            // Cannot include item
+            else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][W];
+}
+
+int main() {
+
+    int val[] = {60, 100, 120};
+    int wt[]  = {10, 20, 30};
+
+    int W = 50;
+
+    int n = sizeof(val) / sizeof(val[0]);
+
+    printf("Maximum value = %d\n", knapsack(W, wt, val, n));
+
+    return 0;
+}
